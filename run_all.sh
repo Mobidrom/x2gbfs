f99000d55d96bb82f0f578f7163e609af6a7a701
john-funk <john.funk@mobidrom.nrw>
2026-04-24 08:28:25 +0200

    refactor: align indentation

--- run_all.sh
+++ run_all.sh
@@ -23,12 +23,12 @@ fi
 missing=''
 [ -n "${RCLONE_CONFIG_S3_ENDPOINT}" ] || missing="${missing} RCLONE_CONFIG_S3_ENDPOINT"
 [ -n "${RCLONE_CONFIG_S3_PROVIDER}" ] || missing="${missing} RCLONE_CONFIG_S3_PROVIDER"
-[ -n "${RCLONE_CONFIG_S3_REGION}" ] || missing="${missing} RCLONE_CONFIG_S3_REGION"
-[ -n "${RCLONE_CONFIG_S3_TYPE}" ] || missing="${missing} RCLONE_CONFIG_S3_TYPE"
+[ -n "${RCLONE_CONFIG_S3_REGION}" ] 	|| missing="${missing} RCLONE_CONFIG_S3_REGION"
+[ -n "${RCLONE_CONFIG_S3_TYPE}" ] 		|| missing="${missing} RCLONE_CONFIG_S3_TYPE"
 [ -n "${RCLONE_CONFIG_S3_ENV_AUTH}" ] || missing="${missing} RCLONE_CONFIG_S3_ENV_AUTH"
-[ -n "${RCLONE_CONFIG_S3_ACL}" ] || missing="${missing} RCLONE_CONFIG_S3_ACL"
-[ -n "${AWS_ACCESS_KEY_ID}" ] || missing="${missing} AWS_ACCESS_KEY_ID"
-[ -n "${AWS_SECRET_ACCESS_KEY}" ] || missing="${missing} AWS_SECRET_ACCESS_KEY"
+[ -n "${RCLONE_CONFIG_S3_ACL}" ] 			|| missing="${missing} RCLONE_CONFIG_S3_ACL"
+[ -n "${AWS_ACCESS_KEY_ID}" ] 				|| missing="${missing} AWS_ACCESS_KEY_ID"
+[ -n "${AWS_SECRET_ACCESS_KEY}" ] 		|| missing="${missing} AWS_SECRET_ACCESS_KEY"
 if [ -n "${missing}" ]; then
 	echo "ERROR: Missing Rclone configuration variable:${missing}. Aborting." >&2
 	exit 2
